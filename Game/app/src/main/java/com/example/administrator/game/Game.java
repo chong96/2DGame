bb0233bf186948ecee6b6a9b42a478d2f7904a8d
Bryan Chong <bchong1096@gmail.com>
2017-06-28 15:23:00 -0400

    running background

--- Game/app/src/main/java/com/example/administrator/game/Game.java
+++ Game/app/src/main/java/com/example/administrator/game/Game.java
@@ -2,12 +2,21 @@ package com.example.administrator.game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Game extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new GamePanel(this));
     }
 }
